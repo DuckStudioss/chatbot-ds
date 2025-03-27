e38038f8726a7d282b785f9adfdb0ec1c80af66c
Duck Studios <info@duckstudios.net>
2025-03-26 20:02:44 -0600

    added async function to work with new library

--- utils/languageDetector.js
+++ utils/languageDetector.js
@@ -1,10 +1,10 @@
+const { franc } = require('franc-min');
+
 const detectLanguage = async (text) => {
-    const franc = await import('franc-min');
-    const langCode = franc.default(text);
-    if (langCode === 'spa') return 'es';
-    if (langCode === 'eng') return 'en';
-    return 'es';
-  };
-  
-  module.exports = { detectLanguage };
-  
+  const langCode = franc(text); // ✅ llamada directa
+  if (langCode === 'spa') return 'es';
+  if (langCode === 'eng') return 'en';
+  return 'es';
+};
+
+module.exports = { detectLanguage };
